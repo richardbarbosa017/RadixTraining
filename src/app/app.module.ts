654bb7a3ef8e3900dd670ff3b2f530df3cb1decb
Richard Barbosa <richard.barbosa@championx.com>
2021-06-02 11:51:32 -0300

    Header, footer and others created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
 import { MainModule } from './components/main/main.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { TodoPageComponent } from './components/todo-page/todo-page.component';
+import { FooterModule } from './shared/footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainModule
+    MainModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/main/main.module.ts
+++ src/app/components/main/main.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { FooterModule } from 'src/app/shared/footer/footer.module';
+import { HeaderModule } from 'src/app/shared/header/header.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
-  ]
+    FooterModule,
+    HeaderModule,
+  ],
+  exports: [MainComponent]
 })
 export class MainModule { }
